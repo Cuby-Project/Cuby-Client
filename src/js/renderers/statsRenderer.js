155a79417da44908adda96a3848127998cdf948e
quentinformatique <quentin161204@gmail.com>
2024-01-22 23:48:27 +0100

    begin grid display for stats

--- src/js/renderers/statsRenderer.js
+++ src/js/renderers/statsRenderer.js
@@ -4,7 +4,6 @@ const NB_SOLVE_SELECT = document.getElementById("displayedNumber");
 const CONTENT = document.getElementById("content");
 
 function displayContent(change = false) {
-    console.log(CONTENT.getAttribute('display'))
     if (CONTENT.getAttribute('display') === "chart") {
         displayChart(CUBE_SELECT.value);
     } else {
@@ -17,61 +16,39 @@ function displayChart(cube) {
     chartAPI.getChart(cube, document.getElementById('myChart'));
 }
 
-async function generateTableContent(cube) {
-    let solves = await solvesDataAPI.getCubeSolves(cube)
-
-    // from the solves we generate the table content with a row per solve
-    // and a column for the solve number, the time, the gap to average, the edit button, the delete button and the date
-    let content = "<thead><tr class='tableTr block'><th class='tableTh'>Solve number</th><th class='tableTh'>Time</th><th class='tableTh'>Gap to average</th><th class='tableTh'>Date</th><th class='tableTh'>Edit</th><th class='tableTh'>Delete</th></tr></thead><tbody class='overflow-auto max-h-[59vh] block'>";
-    let average = await solvesDataAPI.getAverage(cube);
-    let nbSolve = solves.length;
-
-    // if there is no solve yet, we display a message
-    if (nbSolve === 0) {
-        return "<tr class='tableTr block'><td class='tableTd' colspan='6'>No solve yet</td></tr></tbody>";
-    }
-
-    solves.forEach(solve => {
-        let gap = solve.time - average;
-
-        let negative = false;
-        // calculate the gap to average
-        let gapToAverage;
-        if (solve.time > average) {
-            gapToAverage = solve.time - average;
-            negative = true;
-        } else {
-            gapToAverage = average - solve.time;
-        }
-        let gapString = timeAPI.formatDuration(gapToAverage)
-        let row = `<tr class='tableTr block'><td class='tableTd'>${nbSolve}</td><td class='tableTd'>${timeAPI.formatDuration(solve.time)}</td>`;
-
-        if (negative) {
-            row += `<td class='tableTd text-red-500'>${gapString}</td>`;
-        } else {
-            row += `<td class='tableTd text-green-500'>${gapString}</td>`;
-        }
-
-        row += `<td class='tableTd'>${solve.date}</td><td class='tableTd'><button class='text-custom-blue' id='editButton${nbSolve}'><i class='fa-solid fa-edit'></i></button></td><td class='tableTd'><button class='text-red-500' id='deleteButton${nbSolve}'><i class='fa-solid fa-trash'></i></button></td></tr>`;
-        content += row;
-        nbSolve--;
-    });
-
-    return content + "</tbody>";
-
-}
-
 async function displayTable(cube) {
-    let content = await generateTableContent(cube);
     // TODO: change to a grid
+    let solves = await solvesDataAPI.getCubeSolves(cube);
     CONTENT.innerHTML = `
-                        <div class='dark:bg-custom-gray-3 bg-blue-200 rounded-xl h-fit max-h-[65vh] flex items-center justify-center p-4'>
-                            <table class='dark:text-white w-full table-fixed full-width-table' id='solvesHistory'>
-                                ${content}
-                            </table>
-                        </div>
-                        `;
-
+                        <div class="grid grid-cols-5 grid-rows-${solves.length + 1} dark:bg-custom-gray-2 bg-blue-100 rounded-lg shadow-lg p-5 w-full h-[65vh] m-1">
+                            <div class="text-center text-white font-bold">solve number</div>
+                            <div class="text-center text-white font-bold">time</div>
+                            <div class="text-center text-white font-bold">gap to average</div>
+                            <div class="text-center text-white font-bold">scramble</div>
+                            <div class="text-center text-white font-bold">edit</div>
+                            ${solves.map((solve) => {
+                                let average = 0;
+                                solves.forEach((solve) => {
+                                    average += solve.time;
+                                });
+                                average = average / solves.length;
+                                let negative = false;
+                                let gapToAverage;
+                                if (solve.time > average) {
+                                    gapToAverage = solve.time - average;
+                                    negative = true;
+                                } else {
+                                    gapToAverage = average - solve.time;
+                                }
+                                return `
+                                    <div class="text-center text-white grid-cell">${solve.solveNumber}</div>
+                                    <div class="text-center text-white grid-cell">${timeAPI.formatDuration(solve.time)}</div>
+                                    ${negative ? `<div class="text-center grid-cell text-red-500">+ ${timeAPI.formatDuration(gapToAverage)}</div>` 
+                                               : `<div class="text-center grid-cell text-green-500">- ${timeAPI.formatDuration(gapToAverage)}</div>`}
+                                    <div class="text-center grid-cell text-white">${solve.scramble}</div>
+                                    <div class="text-center grid-cell text-white"><i class='fas fa-edit text-custom-blue'></i></div>`;
+                            }).join('')}
+                        }`;
 }
 
 CUBE_SELECT.addEventListener('change', function () {
@@ -81,11 +58,9 @@ CUBE_SELECT.addEventListener('change', function () {
 
 CHANGE_BUTTON.addEventListener('click', function () {
     if (CONTENT.getAttribute('display') === "chart") {
-        console.log('chart to table')
         CONTENT.setAttribute('display', "table");
         CHANGE_BUTTON.innerHTML = "<i class='fa-solid fa-chart-line mx-2'></i> Chart";
     } else {
-        console.log('table to chart')
         CONTENT.setAttribute('display', "chart")
         CHANGE_BUTTON.innerHTML = "<i class='fa-solid fa-table-list mx-2'></i> Table";
     }
